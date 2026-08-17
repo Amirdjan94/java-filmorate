6334b947c81615fafbc840886dc7124a5fc5d4c4
Dmitrii Konstantinov <dimonkent61@yandex.ru>
2026-08-17 14:47:26 +0300

    "Добавлен поиск по названию фильмов и по режиссёру, а также подгрузка режиссеров и жанров вынесена отдельно в методы"

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -7,6 +7,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,6 +40,14 @@ public class FilmController {
             @PathVariable Long directorId,
             @RequestParam String sortBy) {
         return filmService.getByDirector(directorId, sortBy);
+    }
+
+    @GetMapping("/search")
+    public List<Film> searchFilms(
+            @RequestParam String query,
+            @RequestParam String by
+    ) {
+        return filmService.searchFilms(query, by);
     }
 
 }
